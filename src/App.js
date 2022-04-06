5be5554d22d689ab88f2fd8c6476d686e7e8a3b3
Alexis Castellanos <castellanos.alexis@hotmail.com>
2022-04-06 13:03:15 -0500

    modified2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom";
 import { Excercise1 } from "./excecises/Excercise1";
 import { Excercise2 } from "./excecises/Excercise2";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+   return (
       <BrowserRouter>
+         <div>
+            <nav>
+               <ul id="navigation">
+                  <li>
+                     <Link to="/excerciseA">excerciseA</Link>
+                  </li>
+                  <li>
+                     <Link to="/excerciseB">excerciseB</Link>
+                  </li>
+               </ul>
+            </nav>
+         </div>
          <Routes>
             <Route path="/excercise2" element={<Excercise2 />} />
             <Route path="/*" element={
@@ -14,10 +26,10 @@ function App() {
                   <Route path="excerciseA" element={<Excercise1 />} />
                   <Route path="excerciseB" element={<Excercise2 />} />
                </Routes>
-   } />
+            } />
          </Routes>
       </BrowserRouter>
-  );
+   );
 }
 
 export default App;
